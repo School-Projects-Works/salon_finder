6b4067ed1b9ec52b39094414ee74d5d6e808819c
Emmanuel Frimpong <emmanuelfrimpong07@gmail.com>
2025-07-07 18:01:34 +0000

    initial update

--- lib/app/ui/pages/auth_page/auth_page.dart
+++ lib/app/ui/pages/auth_page/auth_page.dart
@@ -1,18 +1,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../controllers/login_controller.dart';
+import '../../../controllers/auth_controller.dart';
 
 
-class LoginPage extends GetView<LoginController> {
+class AuthPage extends GetView<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LoginPage'),
+        title: Text('AuthPage'),
       ),
       body: SafeArea(
-        child: Text('LoginController'),
+        child: Text('AuthController'),
       ),
     );
   }
